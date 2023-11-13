32fab4c9dd13322970f637e1cf46a9fc52796f15
parkjihy <74035930+parkjihy@users.noreply.github.com>
2023-11-13 14:21:18 +0900

    Update review.php

--- Reveiw/review.php
+++ Reveiw/review.php
@@ -80,42 +80,57 @@
     <div class="section">
         <h1>Ranking_review</h1>
         <span>Overall ranking by stars (0-5), reviews (also for each store)</span>
+        <div>
         <?php
-            $servername = "localhost";
-            $username = "root";
-            $password = "";
-            $dbname = "restaurant_data";
-        
-            // MySQL 연결
-            $conn = new mysqli($servername, $username, $password, $dbname);
-        
-            // 연결 확인
-            if ($conn->connect_error) {
-                die("Connection failed: " . $conn->connect_error);
-            }
-        
-            // 각 상점별 평균 랭킹 쿼리
-            $sql = "SELECT store_id, AVG(ranking) as avg_ranking
-                    FROM Reviews
-                    GROUP BY store_id
-                    ORDER BY avg_ranking ASC";
-        
-            $result = $conn->query($sql);
-        
-            // 결과 출력
-            if ($result->num_rows > 0) {
-                echo "<table><tr><th>Store ID</th><th>Average Ranking</th></tr>";
-                while ($row = $result->fetch_assoc()) {
-                    echo "<tr><td>" . $row["store_id"] . "</td><td>" . $row["avg_ranking"] . "</td></tr>";
+        $servername = "localhost";
+        $username = "root";
+        $password = "";
+        $dbname = "restaurant_data";
+
+        // MySQL 연결
+        $conn = new mysqli($servername, $username, $password, $dbname);
+
+        // 연결 확인
+        if ($conn->connect_error) {
+            die("Connection failed: " . $conn->connect_error);
+        }
+
+        // 각 상점별 평균 랭킹 쿼리
+        $sql = "SELECT store_ID, AVG(ranking) as avg_ranking
+                FROM Reviews
+                GROUP BY store_ID
+                ORDER BY avg_ranking ASC";
+
+        $result = $conn->query($sql);
+
+        // 결과 출력
+        if ($result->num_rows > 0) {
+            echo "<table><tr><th>Store ID</th><th>Average Ranking</th><th>Top 3 Comments</th></tr>";
+            while ($row = $result->fetch_assoc()) {
+                $storeID = $row["store_ID"];
+                $avgRanking = $row["avg_ranking"];
+
+                // 각 상점별 상위 3개 코멘트 쿼리
+                $commentSql = "SELECT comments FROM Reviews WHERE store_ID = '$storeID' ORDER BY ranking ASC LIMIT 3";
+                $commentResult = $conn->query($commentSql);
+
+                // 코멘트 출력
+                $comments = [];
+                while ($commentRow = $commentResult->fetch_assoc()) {
+                    $comments[] = $commentRow["comments"];
                 }
-                echo "</table>";
-            } else {
-                echo "No results found.";
+
+                echo "<tr><td>" . $storeID . "</td><td>" . $avgRanking . "</td><td>" . implode(", ", $comments) . "</td></tr>";
             }
-        
-            // 연결 종료
-            $conn->close();
+            echo "</table>";
+        } else {
+            echo "No results found.";
+        }
+
+        // 연결 종료
+        $conn->close();
         ?>
-    </div>
+
+        </div>
 </body>
 </html>
